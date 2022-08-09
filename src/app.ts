fcbf11f8a63c726ebd31772aca524f7088ac38be
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2022-08-09 23:50:38 +0200

    Add node-redis

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,16 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+(async () => {
 // eslint-disable-next-line no-console
-console.log('Hello World');
+  client.on('error', (err) => console.log('Redis Error', err));
+
+  await client.connect();
+
+  await client.set('key', 'value');
+  const value = await client.get('key');
+
+  // eslint-disable-next-line no-console
+  console.log(value);
+})();
