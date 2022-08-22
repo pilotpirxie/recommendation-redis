b242246d59f3e79a81fbff711a63fa9c333c0065
pilotpirxie <10637666+pilotpirxie@users.noreply.github.com>
2022-08-22 17:59:03 +0200

    Initialize first controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,23 @@
-// import { createClient } from 'redis';
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { errorHandler } from './middlewares/errors';
+import resourcesController from './controllers/resources.controller';
 
-// const client = createClient({
-//   password: 'mysecretpassword',
-// });
+dotenv.config();
+const port = process.env.PORT || 3000;
+const app: Express = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/resources', resourcesController);
+
+app.use(errorHandler);
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on port ${port}`);
+});
